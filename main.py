7c63996558148f0de1ce46c6863e4fd0384cfdc6
Oleh Mazur <oleh@four-velocity.com>
2023-04-12 09:16:40 +0200

    Basic functionality

--- main.py
+++ main.py
@@ -1,43 +1,80 @@
 from __future__ import annotations
 
 import json
-from typing import ForwardRef, Self
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel, Field, BaseSettings
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
-# Period = ForwardRef("Period")
-# class Period(BaseModel):
-#     name: str
-#     order: int
-#     start_time: float
-#     end_time: float
-#     periods: list[Period] | None = None
-#
-#
-# class Breakpoint(BaseModel):
-#     group_id: str
-#     item_id: str
-#     type: str
-#     periods: list[Period]
+
+class Settings(BaseSettings):
+    database_url: str
+
+
+settings = Settings()
+
+
+class Period(BaseModel):
+    name: str
+    verbose: str | None = None
+    start_time: float
+    end_time: float
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "some_name",
+                "verbose": "Some Task (Profiling execution)",
+                "start_time": 1681282223.053712,
+                "end_time": 1681283174.048276,
+            }
+        }
+
+
+class Breakpoint(BaseModel):
+    group_id: str = Field()
+    item_id: str
+    type: str | None = None
+    periods: list[Period]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "group_id": "b4827861-48cb-41e0-a907-471f4b2b1e27",
+                "item_id": "abC=12df",
+                "type": "success",
+                "periods": [
+                    {"name": "total", "start_time": 1681281218.3709528, "end_time": 1681283174.048276,},
+                    {"name": "sub_task_a", "start_time": 1681281218.3709528, "end_time": 1681282223.053712,},
+                    {"name": "sub_task_b", "verbose": "Task B (Optional)", "start_time": 1681282223.053712, "end_time": 1681283174.048276,},
+                ]
+            }
+        }
+
+
+class BreakpointResponse(Breakpoint):
+    id: str = Field(default_factory=str, alias="_id")
 
 
 app = FastAPI()
+client = AsyncIOMotorClient(settings.database_url)
 
 
-@app.post("/breakpoints/")
-async def add_breakpoint(breakpoint: dict):
-    print(breakpoint)
-    with open("./data.json", "a") as file:
-        file.write(json.dumps(breakpoint))
-        file.write(",\n")
-    return 200
+def get_db():
+    db = client.app
+    return db
 
-@app.get("/breakpoints/")
-async def list_breakpoints():
-    with open("./data.json") as file:
-        data = file.read()
-    if data.endswith(",\n"):
-        data = data[:-2]
-    dump = data + "]"
 
-    return json.loads(dump)
+@app.post("/breakpoints/", response_model=Breakpoint, tags=["Breakpoints"])
+async def add_breakpoint(breakpoint: Breakpoint, db: AsyncIOMotorDatabase = Depends(get_db)):
+    collection = db.default
+    results = await collection.insert_one(breakpoint.dict())
+    return Breakpoint
+
+
+@app.get("/breakpoints/", response_model=list[Breakpoint], tags=["Breakpoints"])
+async def list_breakpoints(db: AsyncIOMotorDatabase = Depends(get_db)):
+    collection = db.default
+    # results = await collection.find({}).to_list(length=None)
+    results = [i async for i in collection.find({})]
+    return results
+
